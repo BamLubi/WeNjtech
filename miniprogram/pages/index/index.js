ee292df393db731a587acf181862928784c1135a
BamLubi <1126458447@qq.com>
2019-09-07 22:00:42 +0800

    校车线路存储逻辑优化
    增加多站点以及亚青线；优化存储结构；

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -2,6 +2,8 @@
 
 var util = require('../../utils/util.js');
 
+const db = wx.cloud.database();
+
 Page({
 
     /**
@@ -14,12 +16,13 @@ Page({
         selectDate: {}, //用户选择的时间,默认为当日
         busLineShow: [], //用于显示的列表
         hasBus: false, //是否有班车
-        position: ["象山", "校门"],
-        positionStartIndex: 0,
-        positionEndIndex: 1,
+        position: ["校门","东苑","北苑","同和","象山", "亚青"],
+        positionStartIndex: 4,
+        positionEndIndex: 0,
         direction: 0,
         allBusInfo: null,
-        isChangePlace: false
+        isChangePlace: false,
+		dict: []//字典
     },
 
     /**
@@ -59,6 +62,15 @@ Page({
         this.setData({
             todayDate: JSON.parse(JSON.stringify(this.data.selectDate)) //深拷贝对象
         })
+		// 设置字典
+		db.collection('schoolBusTable').doc('08647083-7954-4d1e-932c-6f8002e1c6c6').get({
+			success: function (res) {
+				console.log("下载-字典-信息成功")
+				that.setData({
+					dict: res.data
+				})
+			}
+		})
         // 连接数据库并设置班车信息
         this.connectDB();
     },
@@ -122,34 +134,66 @@ Page({
      */
     connectDB: function() {
         var that = this
-        console.log("准备连接数据库")
         // 显示加载框
         wx.showLoading({
             title: '加载中',
         })
         // 获取信息
-        const db = wx.cloud.database()
-        db.collection('schoolBusTable').doc('f0b862af-d862-4a8a-937a-a3ce4f221fdc').get({
-            success: function(res) {
-                console.log("下载班车信息成功")
-                // 更具星期设置变量
-                if (that.data.selectDate.week == 6 || that.data.selectDate.week == 0) {
-                    // 周末
-                    that.setData({
-                        allBusInfo: res.data.weekendData
-                    })
-                } else {
-                    // 工作日
-                    that.setData({
-                        allBusInfo: res.data.workingDayData
-                    })
-                }
-                // 产生班车列表
-                that.setBusLine()
-                // 隐藏加载框
-                wx.hideLoading()
-            }
-        })
+        // db.collection('schoolBusTable').doc('f0b862af-d862-4a8a-937a-a3ce4f221fdc').get({
+        //     success: function(res) {
+        //         console.log("下载班车信息成功")
+        //         // 更具星期设置变量
+        //         if (that.data.selectDate.week == 6 || that.data.selectDate.week == 0) {
+        //             // 周末
+        //             that.setData({
+        //                 allBusInfo: res.data.weekendData
+        //             })
+        //         } else {
+        //             // 工作日
+        //             that.setData({
+        //                 allBusInfo: res.data.workingDayData
+        //             })
+        //         }
+        //         // 产生班车列表
+        //         that.setBusLine()
+        //         // 隐藏加载框
+        //         wx.hideLoading()
+        //     }
+        // })
+		// 测试新的功能
+		db.collection('schoolBusTable').doc('0ea5f45b-d433-4608-a2b2-ce94309ac44a').get({
+			success: function (res) {
+				console.log("下载-班车-信息成功")
+				let busInfo = null
+				// 更具星期设置变量
+				if (that.data.selectDate.week == 6 || that.data.selectDate.week == 0) {
+					// 周末
+					if(that.data.direction == 0){
+						busInfo = res.data.weekendData.forward
+						console.log("周末-正向")
+					}else{
+						console.log("周末-反向")
+						busInfo = res.data.weekendData.reverse
+					}
+				} else {
+					// 工作日
+					if (that.data.direction == 0) {
+						console.log("工作日-正向")
+						busInfo = res.data.workingDayData.forward
+					} else {
+						console.log("工作日-反向")
+						busInfo = res.data.workingDayData.reverse
+					}
+				}
+				that.setData({
+                    allBusInfo: busInfo
+                })
+				// 产生班车列表
+				that.setBusLine()
+				// 隐藏加载框
+				wx.hideLoading()
+			}
+		})
     },
     /**
      * 产生班车列表
@@ -164,12 +208,9 @@ Page({
             let st = parseInt(parseFloat(data[i].startTime).toFixed(2) * 100);
             let ed = parseInt(parseFloat(data[i].endTime).toFixed(2) * 100);
             // 筛选当前时间之后的班车
-            if (parseInt(ed - nowTime) >= -5) {
-                // 方向一致，或为循环车
-                if (data[i].direction == this.data.direction || data[i].status != 1) {
-                    ans.push(new busLine(data[i]))
-                }
-            }
+			if (parseInt(ed - nowTime) >= -5) {
+				ans.push(new busLine(data[i], this.data.dict))
+			}
         }
         // 赋值
         this.setData({
@@ -190,16 +231,22 @@ Page({
     /**
      * 更改方向
      */
-    changeDirection: function() {
+    ChangeDirection: function() {
+		console.log("更改方向")
         let edIndex = this.data.positionEndIndex
         let stIndex = this.data.positionStartIndex
         this.setData({
             positionStartIndex: edIndex,
             positionEndIndex: stIndex,
-            direction: !this.data.direction
         })
     },
     Search: function() {
+		// 
+		console.log("开始" + this.data.positionStartIndex)
+		console.log("结束" + this.data.positionEndIndex)
+		this.setData({
+			direction: parseInt(this.data.positionEndIndex) - parseInt(this.data.positionStartIndex) < 0 ? 0 : 1
+		})
         // 赋空值
         this.setData({
             busLineShow: []
@@ -208,7 +255,7 @@ Page({
         this.connectDB()
         // 获取班车信息
         this.setBusLine()
-        // 
+        // 隐藏模态框
         this.HideMask()
     },
     /**
@@ -255,8 +302,25 @@ Page({
             ["selectDate.hour"]: parseInt(time[0]),
             ["selectDate.minutes"]: parseInt(time[1])
         })
-
     },
+	/**
+	 * 更改起点
+	 */
+	ChangeStartPosition(e) {
+		console.log("修改起点：" + e.detail.value)
+		this.setData({
+			positionStartIndex: e.detail.value
+		})
+	},
+	/**
+	 * 更改终点
+	 */
+	ChangeEndPosition(e){
+		console.log("修改终点：" + e.detail.value)
+		this.setData({
+			positionEndIndex: e.detail.value
+		})
+	},
     /**
      * 显示遮罩层
      */
@@ -317,19 +381,32 @@ Page({
 })
 
 // 对象构造器
-function busLine(line) {
+function busLine(childLine,dict) {
     // 开始时间
-    let stNum = parseFloat(line.startTime).toFixed(2).split(".");
+	let stNum = parseFloat(childLine.startTime).toFixed(2).split(".");
     this.startTime = stNum[0] + ":" + stNum[1];
-    // 结束时间
-    let edNum = parseFloat(line.endTime).toFixed(2).split(".");
+    
+	// 结束时间
+	let edNum = parseFloat(childLine.endTime).toFixed(2).split(".");
     this.endTime = edNum[0] + ":" + edNum[1];
-    // 备注信息
-    // this.info = line.info;
-    // 方向，0：象山开往校门口，1：校门口开往象山
-    this.direction = line.direction;
+    
+	// 备注信息
+    // this.info = childLine.info;
+    
+	// 方向，0：象山开往校门口，1：校门口开往象山
+    // this.direction = childLine.direction;
+
     // 状态，1：准点发车，3：三车循环，4：四车循环
-    this.status = line.status;
+	this.status = childLine.status;
+
+	//线路ID，0为象山线，1为亚青线
+	this.lineID = childLine.line == 'busLine_1'? 0 : 1 ;
+
+	// 线路名称
+	this.line = dict[childLine.line];
+
+	// 站点名称
+	this.site = dict[childLine.site];
 }
 
 // 对象构造器，构造e.detail.value，用于DateChange()方法的复用
