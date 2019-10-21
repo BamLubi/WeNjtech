48d70a6195a2034dd5687be437277e38b23ae3e4
BamLubi <1126458447@qq.com>
2019-10-21 21:46:45 +0800

    添加tabBar高度；判断用户授权获取个人信息；

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,10 @@
 //app.js
 App({
 	globalData: {
-		systemInfo: null
+		systemInfo: null,
+		tabBarHeight: 49,
+		userInfo: null,
+		hasUserInfo: false,
 	},
     onLaunch: function() {
 		var that = this
@@ -19,6 +22,37 @@ App({
 			success: function (res) {
 				that.globalData.systemInfo = res
 				wx.setStorageSync('systemInfo', res)
+				// 设置tabBar高度
+				if(that.globalData.systemInfo.model=="iPhone X"){
+					that.tabBarHeight = 83
+				}
+			}
+		})
+		// 在已经授权的情况下，获取用户信息
+		wx.getSetting({
+			success: res => {
+				if (res.authSetting['scope.userInfo']) {
+					console.log("[app]: " + "用户已经授权")
+					// 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+					wx.getUserInfo({
+						success: res => {
+							// 可以将 res 发送给后台解码出 unionId
+							that.globalData.userInfo = res.userInfo
+							that.globalData.hasUserInfo = true
+							// console.log("全局用户信息: ", res.userInfo)
+							// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+							// 所以此处加入 callback 以防止这种情况
+							if (that.userInfoReadyCallback) {
+								that.userInfoReadyCallback(res)
+							}
+						},
+						fail: (res) => {
+							console.log("[app]: " + "获取用户信息失败")
+						}
+					})
+				} else {
+					console.log("[app]: " + "用户暂时未授权")
+				}
 			}
 		})
     }
