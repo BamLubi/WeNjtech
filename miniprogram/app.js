847294f763d4212f4682542793e4919d1c460889
BamLubi <1126458447@qq.com>
2020-04-19 18:14:07 +0800

    修改
    改变全局获取userinfo流程;新增localUserInfo和cloudUserInfo

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,14 +1,17 @@
 //app.js
 const cloudFun = require("./promise/wxCloudFun.js")
+const userInfoDB = require("./utils/userInfoDB.js")
 const API = require("./promise/wxAPI.js")
 App({
 	globalData: {
 		systemInfo: null,
 		tabBarHeight: 49,
-		userInfo: null,
-		hasUserInfo: false,
+		cloudUserInfo: null,
+		localUserInfo: null,
 		openid: null,
+		hasUserInfo: false,
 	},
+
 	onLaunch: function () {
 		var that = this
 		// 初始化云开发环境
@@ -31,17 +34,31 @@ App({
 				}
 			}
 		})
+
+		// 获取用户信息
+		this.getUserInfo()
+	},
+
+	/**
+	 * 获取用户信息,包括从云端获取信息
+	 */
+	getUserInfo: function () {
+		var that = this
+		let userInfo =''
 		// 判断用户是否授权
 		API.GetSetting().then(res => {
 			// 获取用户信息
 			return API.GetUserInfo()
 		}).then(res => {
-			that.globalData.userInfo = res.userInfo
+			// that.globalData.userInfo = res.userInfo
+			userInfo = res.userInfo
 			that.globalData.hasUserInfo = true
 			// 获取用户openid
 			return cloudFun.CallWxCloudFun("login", {})
 		}).then(res => {
 			that.globalData.openid = res.openid
+			return userInfoDB.DownLoadUserInfo(res.openid, userInfo)
+		}).then(res => {
 			// 回调广播函数
 			if (that.userInfoReadyCallback) {
 				that.userInfoReadyCallback()
