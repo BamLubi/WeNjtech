c8ce02e3e241bd33f07c2d166089953142f042d2
BamLubi <1126458447@qq.com>
2020-09-23 22:23:46 +0800

    新增考研打野模式，支持按空闲时间最长排序

--- miniprogram/pages/classroom/classroom.js
+++ miniprogram/pages/classroom/classroom.js
@@ -12,8 +12,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    scrollFixHeight: 320,
-    cardCur: 0,
+    scrollFixHeight: 500, // 固定距离
+    cardCur: 0, // 顶部轮播图片标签
     lh: ["全部学区", "厚学楼", "浦江楼", "仁智楼", "同和楼"],
     tab_lh: [1, 0, 0, 0, 0],
     time: ["全部时间", "1-2", "3-4", "5-6", "7-8", "9-10"],
@@ -24,8 +24,9 @@ Page({
     dict: {},
     classroomList: [],
     classroomListLength: 0,
-    isLoading: false,
-    hasMore: true
+    isLoading: false, // 是否正在加载
+    hasMore: true, // 是否有更多数据
+    mode: 0, // 模式选择(默认:0, 考研:1)
   },
 
   /**
@@ -38,11 +39,11 @@ Page({
     let ratio = wx.getSystemInfoSync().pixelRatio
     if (ratio == 3) {
       this.setData({
-        scrollFixHeight: 348,
+        scrollFixHeight: 429,
       })
     } else {
       this.setData({
-        scrollFixHeight: 319,
+        scrollFixHeight: 365,
       })
     }
     // 下载字典
@@ -133,14 +134,14 @@ Page({
     let id = e.currentTarget.dataset.id
     let arr = e.currentTarget.dataset.arr
     // 处理“全部”选项的事项
+    // 选中了除第一个以外的内容
     if (this.data[arr][0] == 1 && id != 0) {
-      // 选中了除第一个以外的内容
       this.setData({
         [arr + '[0]']: 0
       })
     }
+    // 选中全部选项，其他全部变为0
     if (this.data[arr][0] == 0 && id == 0) {
-      // 选中全部选项，其他全部变为0
       for (let i = 1; i < this.data[arr].length; i++) {
         this.setData({
           [arr + '[' + i + ']']: 0
@@ -176,6 +177,9 @@ Page({
     }
   },
 
+  /**
+   * 设置空教室
+   */
   setEmptyClassroom() {
     let that = this
     // 设置加载中
@@ -185,60 +189,79 @@ Page({
     // 处理选项信息
     // 楼号信息
     let building = []
-    for (let i = 0; i < this.data.tab_lh.length; i++) {
-      if (i == 0 && this.data.tab_lh[i] == 1) {
+    for (let i = 0; i < that.data.tab_lh.length; i++) {
+      if (i == 0 && that.data.tab_lh[i] == 1) {
         // 选择全部
-        building = JSON.parse(JSON.stringify(this.data.lh))
-      } else if (this.data.tab_lh[i] == 1) {
+        building = JSON.parse(JSON.stringify(that.data.lh))
+      } else if (that.data.tab_lh[i] == 1) {
         // 单独的楼号
-        building.push(this.data.lh[i])
+        building.push(that.data.lh[i])
       }
     }
     // 如果存在浦江楼选项，则拓展为浦江A楼、浦江B楼、浦江C楼
-    if (this.data.tab_lh[2] == 1 || this.data.tab_lh[0] == 1){
+    if (that.data.tab_lh[2] == 1 || that.data.tab_lh[0] == 1) {
       building.push("浦江A楼")
       building.push("浦江B楼")
       building.push("浦江C楼")
     }
     // 时间信息
     let time = []
-    for (let i = 0; i < this.data.tab_time.length; i++) {
-      if (i == 0 && this.data.tab_time[i] == 1) {
+    for (let i = 0; i < that.data.tab_time.length; i++) {
+      if (i == 0 && that.data.tab_time[i] == 1) {
         // 选择全部
         time = [2, 4, 6, 8, 10]
-      } else if (this.data.tab_time[i] == 1) {
+      } else if (that.data.tab_time[i] == 1) {
         // 单独的楼号
         time.push(2 * i)
       }
     }
     // 楼层信息
     let lc = []
-    for (let i = 0; i < this.data.tab_lc.length; i++) {
-      if (i == 0 && this.data.tab_lc[i] == 1) {
+    for (let i = 0; i < that.data.tab_lc.length; i++) {
+      if (i == 0 && that.data.tab_lc[i] == 1) {
         // 选择全部
         lc = [1, 2, 3, 4, 5, 6]
-      } else if (this.data.tab_lc[i] == 1) {
+      } else if (that.data.tab_lc[i] == 1) {
         // 单独的楼层
         lc.push(i)
       }
     }
 
     // 网络请求
-    classroomDB.DownLoadClassroom(building, time, lc, this.data.classroomListLength).then(res => {
-      let length = this.data.classroomListLength + res.data.length
+    classroomDB.DownLoadClassroom(building, time, lc, that.data.classroomListLength, that.data.mode).then(res => {
+      let length = that.data.classroomListLength + res.length
       // 没有更多数据
-      if (res.data.length == 0) {
+      if (res.length == 0) {
         that.setData({
           hasMore: false
         })
       }
       // 如果length==0则代表没有数据，不另设变量
-      let list = that.data.classroomList.concat(res.data)
+      let list = that.data.classroomList.concat(res)
       that.setData({
         classroomList: list,
         classroomListLength: length,
         isLoading: false
       })
     })
+  },
+
+  /**
+   * 切换模式
+   */
+  changeMode() {
+    // 更改mode
+    this.setData({
+      mode: !this.data.mode
+    })
+    console.log("[查询空闲教室] 模式", this.data.mode)
+    // 重置查询参数
+    this.data.hasMore = true
+    this.data.classroomList = []
+    this.data.classroomListLength = 0
+    this.data.tab_lc[0] = 1
+    this.data.tab_time[0] = 1
+    // 获取新的数据
+    this.setEmptyClassroom()
   }
 })
--- miniprogram/utils/classroomDB.js
+++ miniprogram/utils/classroomDB.js
@@ -3,28 +3,73 @@
 const Promise = require('../promise/es6-promise.min.js')
 const db = wx.cloud.database()
 const _ = db.command
+const $ = db.command.aggregate
 
-function DownLoadClassroom(lh, time, lc, length) {
+/**
+ * 
+ * @param {*} lh 
+ * @param {*} time 
+ * @param {*} lc 
+ * @param {*} length 
+ * @param {boolean} mode 模式，默认为false，考研为true
+ */
+function DownLoadClassroom(lh, time, lc, length, mode) {
   return new Promise(function (resolve, reject) {
-    db.collection('weNjtech-classroom')
-      .where({
-        lh: _.in(lh),
-        emptyTime: _.in(time),
-        lc: _.in(lc)
-      })
-      .orderBy('name', 'asc')
-      .skip(length)
-      .limit(10)
-      .get({
-        success: res => {
-          console.log('[云数据库] [GET] [空教室信息] success: ', res)
-          resolve(res)
-        },
-        fail: err => {
-          console.error('[云数据库] [GET] [空教室信息] fail: ', err)
-          reject(err)
-        }
-      })
+    console.log('[云数据库] [GET] [空教室信息] 模式: ', mode)
+    if (!mode) {
+      // 默认模式
+      db.collection('weNjtech-classroom')
+        .where({
+          lh: _.in(lh),
+          emptyTime: _.in(time),
+          lc: _.in(lc)
+        })
+        .orderBy('name', 'asc')
+        .skip(length)
+        .limit(10)
+        .get({
+          success: res => {
+            console.log('[云数据库] [GET] [空教室信息] success: ', res)
+            resolve(res.data)
+          },
+          fail: err => {
+            console.error('[云数据库] [GET] [空教室信息] fail: ', err)
+            reject(err)
+          }
+        })
+    } else if (mode) {
+      // 考研模式
+      db.collection('weNjtech-classroom')
+        .aggregate()
+        .project({
+          _id: true,
+          cdlb_id: true,
+          lh: true,
+          name: true,
+          emptyTime: true,
+          lc: true,
+          count: $.size('$emptyTime')
+        })
+        .match({
+          lh: _.in(lh),
+          lc: _.in(lc)
+        })
+        .sort({
+          count: -1
+        })
+        .skip(length)
+        .limit(10)
+        .end({
+          success: res => {
+            console.log('[云数据库] [GET] [空教室信息] success: ', res)
+            resolve(res.list)
+          },
+          fail: err => {
+            console.error('[云数据库] [GET] [空教室信息] fail: ', err)
+            reject(err)
+          }
+        })
+    }
   });
 }
 
