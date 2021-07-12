c0b6659a0e6d176346d8eae7969820b0ad10c79e
小陆斑比 <1126458447@qq.com>
2021-07-12 13:02:15 +0800

    删除 手机信息
    全局不再保存手机信息，需要的时候直接调用

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,16 +4,14 @@ const userInfoDB = require("./utils/userInfoDB.js")
 const API = require("./promise/wxAPI.js")
 App({
 	globalData: {
-		systemInfo: null, // 系统信息
-		tabBarHeight: 49, // 底部tabBar高度
 		cloudUserInfo: null, // 云端用户信息
 		localUserInfo: null, // 本地用户信息
 		openid: null, // 用户openid
 		hasUserInfo: null, // true代表有数据,false代表没数据,null代表还在请求
-		systemInfo: null, // 记录用户手机系统
 	},
 
 	onLaunch: function () {
+		console.time("A")
 		let that = this
 
 		// 初始化云开发环境
@@ -26,23 +24,10 @@ App({
 			})
 		}
 
-		// 获取手机信息
-		wx.getSystemInfo({
-			success: function (res) {
-				that.globalData.systemInfo = res
-				// 异步存入缓存
-				wx.setStorageSync('systemInfo', res)
-				// 设置系统
-				that.globalData.system = res.system.split(" ")[0].toUpperCase()
-				// TODO: 设置tabBar高度，用于班车页面的下滑监听，需要改善
-				if (that.globalData.systemInfo.model == "iPhone X") {
-					that.globalData.tabBarHeight = 83
-				}
-			}
-		})
-
 		// 准备用户信息
 		that.getUserInfo()
+
+		console.timeEnd("A")
 	},
 
 	/**
