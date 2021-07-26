e1d3f60ce5b372b2cec0b5f80be47e739cd781b8
小陆斑比 <1126458447@qq.com>
2021-07-26 15:00:39 +0800

    新增UUID生成算法

--- miniprogram/utils/util.js
+++ miniprogram/utils/util.js
@@ -1,6 +1,6 @@
 
 /**
- * 格式化时间
+ * 格式化时间输出格式：YYYY-MM-DD HH-MM-SS
  * @param {Date} date 
  */
 const formatTime = date => {
@@ -11,14 +11,33 @@ const formatTime = date => {
     const minute = date.getMinutes()
     const second = date.getSeconds()
 
-    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+    return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
+/**
+ * 补齐数字为2位
+ * @param {number} n
+ * @return {string}
+ */
 const formatNumber = n => {
     n = n.toString()
     return n[1] ? n : '0' + n
 }
 
+/**
+ * 生成16位UUID
+ * @return {string}
+ */
+function makeUUID(){
+    let a,b,c;
+    [a,b,c] = [a,b,c].map(()=>{
+        return Math.floor(Math.random()*10);
+    })
+    const time = new Date().getTime();
+    return '' + a + time + b + c;
+}
+
 module.exports = {
-    formatTime: formatTime
+    formatTime: formatTime,
+    makeUUID: makeUUID
 }
