f0b2e85fefe65b99670f0661720e6ecbe7c19532
小陆斑比 <1126458447@qq.com>
2021-09-01 14:33:19 +0800

    完善课表功能

---
+++ miniprogram/db/kebiaoDB.js
@@ -0,0 +1,77 @@
+// miniprogram/db/zutuan_db.js
+// 对应云数据库中 zutuan 集合
+const Promise = require('../promise/es6-promise.min.js')
+const CloudDB = require("../promise/wxCloudDB.js")
+const API = require("../promise/wxAPI.js")
+const {
+    date2YMD
+} = require('../utils/dateControl.js')
+const db = wx.cloud.database()
+const _ = db.command
+
+const CollectionName = "weNjtech-kebiao";
+
+/**
+ * 获取课表信息
+ * @param {*} openid
+ */
+function getKebiaoList(openid) {
+    return new Promise(function (resolve, resolve2, reject) {
+        db.collection(CollectionName)
+            .where({
+                _openid: openid
+            })
+            .orderBy('year', 'desc')
+            .get({
+                success: res => {
+                    if (res.data.length != 0) {
+						console.log(`[云数据库] [GET] [课表] success: `, res.data);
+						resolve(res);
+					} else if (res.data.length == 0) {
+						console.log(`[云数据库] [GET] [课表] success: NULL`);
+						resolve2(res);
+					}
+                },
+                fail: err => {
+                    console.error('[云数据库] [GET] [课表] fail: ', err)
+                    reject(err)
+                }
+            })
+    });
+}
+
+/**
+ * 调用服务器接口爬取数据
+ * @param {*} openid 
+ */
+function crawlKebiao(openid) {
+    return new Promise(function (resolve, reject) {
+        // 1. 获取用户学号和密码
+        let app = getApp()
+        let username = app.globalData.localUserInfo.stuNum
+        let password = app.globalData.localUserInfo.stuPwd
+        // 2. 构造请求信息
+        let post_data = {
+            username: username,
+            password: password,
+            openid: openid
+        }
+        let header = {
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+        API.Request('https://dev.bamlubi.cn/weNjtech/get_kebiao.php', post_data, 'POST', header, '调用服务器获取课表').then(res => {
+            if (res.code == 200) {
+                resolve(res.data)
+            } else {
+                throw new Error("服务器获取课表失败")
+            }
+        }).catch(err => {
+            reject(err)
+        })
+    })
+}
+
+module.exports = {
+    getKebiaoList,
+    crawlKebiao
+}
--- miniprogram/pages/kebiao/kebiao.js
+++ miniprogram/pages/kebiao/kebiao.js
@@ -1,103 +1,214 @@
 // pages/kebiao/kebiao.js
+const cloudDB = require("../../promise/wxCloudDB.js");
+const {
+    getKebiaoList,
+    crawlKebiao
+} = require("../../db/kebiaoDB.js");
+const cloudFun = require("../../promise/wxCloudFun.js")
+const cloudStore = require("../../promise/wxCloudStore.js")
+const API = require("../../promise/wxAPI.js")
+const app = getApp();
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        current: 24,// 当前swiper被选中的序号，0开始，非显示出来的选中
-        select: 1,// 被选中的序号，1开始，显示出来的选中
+        current: 24, // 当前swiper被选中的序号，0开始，非显示出来的选中
+        select: 1, // 被选中的周数，1开始，显示出来的选中
         week: [{
             id: 1,
             name: "周一"
-        },{
+        }, {
             id: 2,
             name: "周二"
-        },{
+        }, {
             id: 3,
             name: "周三"
-        },{
+        }, {
             id: 4,
             name: "周四"
-        },{
+        }, {
             id: 5,
             name: "周五"
-        },{
+        }, {
             id: 6,
             name: "周六"
-        },{
+        }, {
             id: 7,
             name: "周日"
         }],
-        weekSelect: 1
+        weekSelect: 1, // 星期
+        kebiaoList: [],
+        showList: [],
+        year: '2021-2022',
+        term: '一',
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        let that = this
+        // 判断当前第几周
+        let start_date = new Date("2021-08-30 00:00:00")
+        let now_date = new Date()
+        let term_week = parseInt((now_date.getTime() - start_date.getTime()) / (86400000 * 7) + 1)
+        term_week = term_week < 20 ? term_week : 20
+        // 判断星期几
+        let week = now_date.getDay()
+        week = week ? week : 7
+        // 更新视图
+        this.setData({
+            weekSelect: week,
+            select: term_week,
+            current: (term_week - 3 + 26) % 26
+        })
+        // 判断是否有用户的学号和密码信息
+        if (app.globalData.localUserInfo.stuNum == "" || app.globalData.localUserInfo.stuPwd == "") {
+            API.ShowModal('缺少信息', '请至"我的页面/编辑资料"完善您的学号和教务处密码，将用于获取您的课表！点击确定将自动跳转', false).then(() => {
+                wx.redirectTo({
+                    url: '/mePackage/pages/info/info',
+                })
+            })
+        } else {
+            wx.showLoading({
+                title: '加载中'
+            })
+            // 获取云端数据
+            getKebiaoList(app.globalData.openid).then(res => {
+                // 云端有数据
+                // 数据可能过期，判断是否为当前学期
+                if (that.isExpire(res.data[0].year, res.data[0].term)){
+                    console.log("数据库课表未过期");
+                    return res.data[0]
+                }else{
+                    console.log("数据库课表过期");
+                    wx.hideLoading().then(() => {
+                        API.ShowToast('课表过期,重新爬取', 'none', 2000).then(() => {
+                            wx.showLoading({
+                                title: '教务处爬取中'
+                            })
+                        })
+                    })
+                    return crawlKebiao(app.globalData.openid)
+                }
+            }, () => {
+                console.log("数据库课表无信息");
+                // 云端无数据,服务器端爬取数据
+                wx.hideLoading().then(() => {
+                    API.ShowToast('初次加载,耗时较长', 'none', 2000).then(() => {
+                        wx.showLoading({
+                            title: '教务处爬取中'
+                        })
+                    })
+                })
+                return crawlKebiao(app.globalData.openid)
+            }).then(res => {
+                console.log("设置数据");
+                wx.hideLoading().then(() => {
+                    API.ShowToast('成功', 'success')
+                })
+                // 设置课表数据
+                that.setKebiaoList(res)
+                // 过滤数据
+                that.filterList()
+            }).catch(err => {
+                console.log("加载数据失败", err);
+                wx.hideLoading().then(() => {
+                    API.ShowToast('失败', 'error')
+                })
+            })
+        }
     },
 
     /**
-     * 生命周期函数--监听页面初次渲染完成
+     * 转发
      */
-    onReady: function () {
+    onShareAppMessage() {
+        return {
+            title: '我的课表',
+            path: '/pages/kebiao/kebiao',
+        }
+    },
 
+    isExpire: function(year, term){
+        // 获取当前年、月
+        let now_year = new Date().getFullYear()
+        let now_month = new Date().getMonth() + 1
+        let now_term = 3
+        // 如果月份在1-7月，则年份减1
+        if (now_month >= 1 && now_month <= 7){
+            now_year -= 1
+        }
+        // 如果月份在2-7则为第二学期
+        if(now_month >= 2 && now_month <= 7){
+            now_term = 12
+        }
+        // 判断数据是否过期
+        return now_year == year && now_term == term
     },
 
     /**
-     * 生命周期函数--监听页面显示
+     * 设置全局课表信息
+     * @param {*} res 
      */
-    onShow: function () {
-
+    setKebiaoList: function (res) {
+        this.setData({
+            year: '' + res.year + '-' + parseInt(res.year + 1),
+            term: res.term == 3 ? '一' : '二'
+        })
+        if (res.data.length == 0) {
+            return
+        } else {
+            this.data.kebiaoList = res.data
+        }
     },
 
     /**
-     * 生命周期函数--监听页面隐藏
+     * 从所有数据中过滤出要展示的数据
+     * @param {*} term_week 周数
+     * @param {*} week 星期
      */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    filterList: function () {
+        // 过滤数据
+        let term_week = this.data.select
+        let week = this.data.weekSelect
+        console.log("过滤数据", term_week, week)
+        let _list = this.data.kebiaoList.filter(function (x) {
+            if (x["week"] == week && x["term_week"].indexOf(term_week) != -1) {
+                return true
+            } else {
+                return false
+            }
+        })
+        // 根据每日上课时间排序升序
+        let compare = function (x, y) {
+            if (x["time"][0] < y["time"][0]) {
+                return -1;
+            } else if (x["time"][0] > y["time"][0]) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        this.setData({
+            showList: _list.sort(compare)
+        })
     },
 
     SwiperChange: function (e) {
         this.setData({
             select: (parseInt(e.detail.current) + 2) % 26 + 1
         })
+        // 过滤数据
+        this.filterList()
     },
 
     /**
      * 选择周数
      */
-    SelectWeeks: function(e){
+    SelectWeeks: function (e) {
         this.setData({
             current: (e.currentTarget.dataset.id - 3 + 26) % 26,
         })
@@ -106,9 +217,11 @@ Page({
     /**
      * 选择星期
      */
-    SelectWeek: function(e){
+    SelectWeek: function (e) {
         this.setData({
             weekSelect: e.currentTarget.dataset.id
         })
+        // 过滤数据
+        this.filterList()
     }
 })
